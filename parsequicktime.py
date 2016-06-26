ded25f175abde53d44523c3c18cb3cf3afa405d2
Ryota Tomioka <tomioka@ttic.edu>
2016-06-26 05:56:48 +0100

    Initial commit.

---
+++ parsequicktime.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+
+# This program is free software. It comes without any warranty, to the extent
+# permitted by applicable law. You can redistribute it and/or modify it under
+# the terms of the Do What The Fuck You Want To Public License, Version 2, as
+# published by Sam Hocevar. See http://sam.zoy.org/wtfpl/COPYING for more
+# details.
+
+# Some useful resources:
+# - http://atomicparsley.sourceforge.net/mpeg-4files.html
+# - http://developer.apple.com/library/mac/#documentation/QuickTime/QTFF/QTFFChap2/qtff2.html
+# - http://www.sno.phy.queensu.ca/~phil/exiftool/TagNames/QuickTime.html
+
+import datetime
+import os.path
+import struct
+import iso8601
+import pytz
+
+EPOCH_ADJUSTER = 2082844800
+
+def adjust_epoch_time(time):
+    return datetime.datetime.utcfromtimestamp(time - EPOCH_ADJUSTER)
+
+def parse_mvhd(f, length):
+    fmt='>4xII'
+    ctime, mtime = struct.unpack(fmt, f.read(12))
+    # Ignore the rest
+    f.read(length-12)
+    created_time = adjust_epoch_time(ctime)
+    modified_time = adjust_epoch_time(mtime)
+    print 'Created at %s: (%d)' %(created_time, ctime)
+    print 'Modified at %s: (%d)' %(modified_time, mtime)
+
+    return modified_time.replace(tzinfo=pytz.utc)
+
+def parse_meta_keys(f, length):
+    fmt='>B3xI'
+    version, entry_count = struct.unpack(fmt, f.read(8))
+    # print 'version={}, entry_count={}'.format(version, entry_count)
+    keys=[]
+    for i in range(entry_count):
+        size, keyns = struct.unpack('>II', f.read(8))
+        size -=8
+        key_value = struct.unpack('>'+'%ds'%size, f.read(size))
+        keys.append(key_value[0])
+        # print 'key=%s' % key_value
+    return keys
+
+def parse_meta_ilst(f, length, keys):
+    n=0
+    while n < length:
+        item_atom_len, key=struct.unpack('>II', f.read(8))
+        al, adata=struct.unpack('>I4s', f.read(8))
+        type, locale, val=struct.unpack('>I4s%ds'%(al-16), f.read(al-8))
+        print key, type, locale, val
+        if keys[int(key)-1]=='com.apple.quicktime.creationdate':
+            ctime=iso8601.parse_date(val)
+        n += item_atom_len
+    return ctime.tzinfo
+
+def parse(f, length, atom, depth, verbose=False):
+    modified_time=None
+    tzinfo=None
+    keys=[]
+    n=0
+    while n < length:
+        al, an = struct.unpack(">I4s", f.read(8))
+        an = an.decode()
+        if verbose:
+            print("{}Atom: {} ({} bytes)".format('-'*depth, an, al))
+        if an=='moov':
+            modified_time = parse(f, al-8, an, depth+1)
+        elif an=='meta':
+            tzinfo = parse(f, al-8, an, depth+1)
+        elif an=='mvhd':
+            modified_time = parse_mvhd(f, al-8)
+        elif an=='keys':
+            keys=parse_meta_keys(f, al-8)
+        elif an=='ilst' and 'com.apple.quicktime.creationdate' in keys:
+            tzinfo = parse_meta_ilst(f, al-8, keys)
+        else:
+            f.read(al-8)
+        n += al
+    if atom=='meta':
+        # Inside 'meta'
+        return tzinfo
+    elif atom=='moov':
+        # Inside 'moov'
+        if tzinfo is not None:
+            return modified_time.astimezone(tzinfo)
+        else:
+            return modified_time
+    elif depth==0:
+        # Root
+        return modified_time
+    else:
+        return None
+
+def get_local_modified_time(fn):
+    fsize = os.path.getsize(fn)
+    with open(fn, "rb") as f:
+        mtime_local=parse(f, fsize, 'root', 0, True)
+    return mtime_local
---
+++ uploader.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+from datetime import datetime
+import glob
+
+import exifread
+import parsequicktime
+import dropbox
+
+config={}
+with open('config') as f:
+    for line in f:
+        if '=' not in line:
+            continue
+        k, v=line.split('=')
+        config[k]=v.strip('\n')
+
+def format_file_name(date, ext):
+    return date_taken.strftime('%Y-%m-%d %H.%M.%S')+'.'+ext
+
+def format_original_dest(date, ext):
+    return config['camera_upload_folder']+'/'+format_file_name(date, ext)
+
+def format_final_dest(date, ext):
+    filename=format_file_name(date, ext)
+    final_dest_format = '/Photos/%d/%02d/'+filename
+    return final_dest_format % (date.year, date.month)
+
+def check_path_exist(dbx, path):
+    try:
+        md=dbx.files_get_metadata(path)
+        return md
+    except dropbox.exceptions.ApiError:
+        return None
+
+def getext(fn):
+    return fn.split('.')[-1].lower()
+
+if __name__ == '__main__':
+    dir = sys.argv[1]
+    dbx=dropbox.Dropbox(config['token'])
+
+    jpeg_file_exts=['jpg']
+    mov_file_exts=['mov']
+    files=[]
+    for dirpath, dirs, fns in os.walk(dir):
+        files += [dirpath+'/'+fn for fn in fns
+                  if getext(fn) in jpeg_file_exts
+                  or getext(fn) in mov_file_exts]
+    print len(files)
+    que = {}
+    for file in files:
+        print file
+        ext=getext(file)
+        if ext in jpeg_file_exts:
+            with open(file,'rb') as f:
+                tags=exifread.process_file(f, details=False)
+            if 'Image DateTime' not in tags:
+                print 'Exif does not contain datetime.'
+                continue
+            date_taken=datetime.strptime(tags['Image DateTime'].printable, '%Y:%m:%d %H:%M:%S')
+        elif ext in mov_file_exts:
+            date_taken=parsequicktime.get_local_modified_time(file)
+        orig_path = format_original_dest(date_taken, ext)
+        final_path = format_final_dest(date_taken, ext)
+        # print 'orig_path=%s' % orig_path
+        # print 'final_path=%s' % final_path
+        if check_path_exist(dbx, orig_path) is not None:
+            print 'Found %s' % orig_path
+            continue
+        if check_path_exist(dbx, final_path) is not None:
+            print 'Found %s' % final_path
+            continue
+        if getext(file) in mov_file_exts:
+            search_pattern = date_taken.strftime('%Y-%m-%d')+'*.'+ext
+            out=dbx.files_search(config['camera_upload_folder'], search_pattern)
+            local_file_size = os.path.getsize(file)
+            matches = [m for m in out.matches if m.metadata.size==local_file_size]
+            if len(matches)>0:
+                m=matches[0]
+                print 'Found %s with the same size' % m.metadata.name
+                continue
+        print '%s does not exist.' % os.path.basename(file)
+        que[final_path]=file # there might be more than one file with the same content
+
+    if len(que)==0:
+        sys.exit(0)
+
+    num_jpegs=len([k for k in que.keys() if getext(k) in jpeg_file_exts])
+    num_movs=len([k for k in que.keys() if getext(k) in mov_file_exts])
+    res=raw_input('%d files (%d images and %d movies) to be uploaded. Proceed (y/n)?' % (len(que), num_jpegs, num_movs))
+    if res=='y':
+        for path in que:
+            file = que[path]
+            folder_path = '/'.join(path.split('/')[:-1])
+            if check_path_exist(dbx, folder_path) is None:
+                dbx.files_create_folder(folder_path)
+            print 'Uploading %s as %s' % (file, path)
+            with open(file,'rb') as f:
+                dbx.files_upload(f, path)
